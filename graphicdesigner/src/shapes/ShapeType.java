fb7e7f65ccf059239a43298660d380123b09433e
Benjamin-C <bencrall@gmail.com>
2019-10-07 09:17:11 -0700

    Removed getShapeTypeFromString, replaced by enum.valueOf

--- graphicdesigner/src/shapes/ShapeType.java
+++ graphicdesigner/src/shapes/ShapeType.java
@@ -10,19 +10,4 @@ EMPTY_TRIANGLE_H,
 SOLID_TRIANGLE_V,
 EMPTY_TRIANGLE_V,
 LINE;
-	
-	public static ShapeType getShapeTypeFromString(String s) {
-		switch(s.toUpperCase()) {
-		case "SOLID_RECTANTLE": return SOLID_RECTANTLE;
-		case "EMPTY_RECTANGLE": return EMPTY_RECTANGLE;
-		case "SOLID_ELIPSE": return SOLID_ELIPSE;
-		case "EMPTY_ECLIPSE": return EMPTY_ECLIPSE;
-		case "SOLID_TRIANGLE": return SOLID_TRIANGLE_H;
-		case "EMPTY_TRIANGLE": return EMPTY_TRIANGLE_H;
-		case "SOLID_TRIANGLE_V": return SOLID_TRIANGLE_V;
-		case "EMPTY_TRIANGLE_V": return EMPTY_TRIANGLE_V;
-		case "LINE": return LINE;
-		}
-		return null;
-	}
 }
